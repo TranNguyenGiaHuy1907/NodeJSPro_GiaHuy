ec2e97ac280632fd4cd368414425b138cf73ec33
Tranhuy2004 <giahuy.trannguyen1907@gmail.com>
2026-03-12 22:41:20 +0700

    update code ts

---
+++ app.ts
@@ -0,0 +1,16 @@
+//const express = require("express");
+import express from "express";
+
+
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!!!!!!!!")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`My app is running on PORT: ${PORT} `)
+})
