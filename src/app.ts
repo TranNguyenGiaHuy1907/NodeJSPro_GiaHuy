09e5d4f2461136bb7d48ac8ae83f0a563204fbe1
Tranhuy2004 <giahuy.trannguyen1907@gmail.com>
2026-03-16 00:20:00 +0700

    update code

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,17 @@ const PORT = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 
-//config routes
-webRoutes(app);
+//config req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //config static files: image/css/js
 app.use(express.static('public'));
+
+//config routes
+webRoutes(app);
+
+
 
 app.listen(PORT, () => {
     console.log(`My app is running on PORT: ${PORT} `)
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,19 +1,12 @@
 import express, { Express } from "express";
+import { getCreateUserPage, getHomePage, postCreateUser } from "../controllers/user.controller";
 
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
-    router.get("/", (req, res) => {
-        res.render("home.ejs")
-    });
-
-    router.get("/giahuy", (req, res) => {
-        res.send("Hello GiaHuyy")
-    });
-
-    router.get("/abc", (req, res) => {
-        res.send("ABC")
-    });
+    router.get("/", getHomePage);
+    router.get("/create-user", getCreateUserPage);
+    router.post("/handle-create-user", postCreateUser);
 
     app.use("/", router);
 
